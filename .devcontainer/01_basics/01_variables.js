a6e3d4f78e6b5e56951da2283d362db28c45ad22
Urvi <110280217+Urvashi189@users.noreply.github.com>
2025-05-08 19:48:11 +0000

    commit config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 270902
+let accountEmail = "urvashi2279@gmail.com"
+var accountPassword = "124578"
+accountCity = "Mumbai"
+
+//accountId = 236758//
+
+accountEmail = "bot101@gmail.com"
+accountPassword = "009988"
+accountCity = "Pune"
+accountState = "Maharashtra"
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
